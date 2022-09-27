0b5121505b8a5d2166ecba47804b9fbafa67bb7d
NielsRahder <N.J.Rahder@tilburguniversity.edu>
2022-09-27 14:14:36 +0200

    download data

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -1,8 +1,34 @@
-# Download dataset 1
-# dir.create('./data/dataset1')  # Uncomment if need to create directory with R
-download.file('https://rgreminger.github.io/files/dataset1.csv','./data/dataset1/dataset1.csv')
+library(readr)
 
-# Download dataset 2
-# dir.create('./data/dataset2')  # Uncomment if need to create directory with R
-download.file('https://rgreminger.github.io/files/dataset2.csv','./data/dataset2/dataset2.csv')
+#create data repository 
+#dir.create('./data')  
 
+
+#download and load data 
+#Dallas
+download.file(url = 'http://data.insideairbnb.com/united-states/tx/dallas/2022-09-14/visualisations/listings.csv',
+              destfile = './data/Dallas_listings.csv')
+download.file(url = 'http://data.insideairbnb.com/united-states/tx/dallas/2022-09-14/visualisations/reviews.csv',
+              destfile = './data/Dallas_reviews.csv')
+
+Dallas_listings <- read_csv('data/Dallas_listings.csv')
+Dallas_reviews <- read_csv('data/Dallas_reviews.csv')
+
+#Mexico
+download.file(url = 'http://data.insideairbnb.com/united-states/il/chicago/2022-09-14/visualisations/listings.csv',,
+              destfile = './data/Chicago_listings.csv')
+
+download.file(url = 'http://data.insideairbnb.com/united-states/il/chicago/2022-09-14/visualisations/reviews.csv',
+              destfile = '.data/Chicago_reviews.csv')
+
+Chicago_listings <- read_csv('data/Chicago_listings.csv')
+Chicago_reviews <- read_csv('data/Chicago_reviews.csv')
+
+#mexico
+download.file(url = 'http://data.insideairbnb.com/mexico/df/mexico-city/2022-06-21/visualisations/listings.csv',
+              destfile = './data/Mexico_listings.csv')
+download.file(url = 'http://data.insideairbnb.com/mexico/df/mexico-city/2022-06-21/visualisations/reviews.csv',
+              destfile = './data/Mexico_reviews.csv')
+
+Mexico_listings <- read_csv('data/Mexico_listings.csv')
+Mexico_reviews <- read_csv('data/Mexico_reviews.csv')
