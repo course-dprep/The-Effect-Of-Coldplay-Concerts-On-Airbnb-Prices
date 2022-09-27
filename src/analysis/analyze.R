358f4a45718aef7334efe593c1a750e758ecea82
NielsRahder <N.J.Rahder@tilburguniversity.edu>
2022-09-27 14:17:05 +0200

    first analysis

--- src/analysis/analyze.R
+++ src/analysis/analyze.R
@@ -1,11 +1,9 @@
-# load 
-load("./gen/analysis/input/data_cleaned.RData")
+#shape of the regression 
+install.packages('fixest')
+library('fixest')
 
-# Estimate model 1 
-m1 <- lm(V1 ~ V3 + V4,df_cleaned)
-
-# Estimate model 2 
-m2 <- lm(V1 ~ V3 + V4 + V5 , df_cleaned)
-
-# Save results
-save(m1,m2,file="./gen/analysis/output/model_results.RData")
+reg1 <- feols(price ~ distance + 
+                type + 
+                distance:type, 
+              data = Dallas_listings)
+tidy(reg1, conf.int = TRUE)
