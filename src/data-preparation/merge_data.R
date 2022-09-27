aa76bab92686a676cb0f363a284ca580d110919a
thomasvandijk98 <103581370+thomasvandijk98@users.noreply.github.com>
2022-09-27 15:51:35 +0200

    Update merge_data.R

--- src/data-preparation/merge_data.R
+++ src/data-preparation/merge_data.R
@@ -12,13 +12,20 @@ Dallas_reviews <- Dallas_reviews %>%
 
 Dallas <- left_join(Dallas_listings, Dallas_reviews, by = 'id')
 
+# Load datasets into R
+Chicago_listings <- read_csv('data/Chicago_listings.csv.gz')
+Chicago_reviews <- read_csv('data/Chicago_reviews.csv.gz')
+
+#join datasets
+Chicago_reviews <- Chicago_reviews %>%
+  rename('id' = listing_id)
+
+Chicago <- left_join(Chicago_listings, Chicago_reviews, by = 'id')
+View(Chicago)
+
 # Load datasets into R 
 Mexico_listings <- read_csv('data/Mexico_listings.csv.gz')
 Mexico_reviews <- read_csv('data/Mexico_reviews.csv.gz')
-
-install.packages('dplyr')
-library(dplyr)
-
 
 #join datasets
 Mexico_reviews <- Mexico_reviews %>%
