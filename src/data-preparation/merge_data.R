cb6cf77a9a1ccc96ec22fbf39b2be1a6e04344f5
EvelinedeVeld <38066744+EvelinedeVeld@users.noreply.github.com>
2022-09-27 15:47:25 +0200

    Update merge_data.R

--- src/data-preparation/merge_data.R
+++ src/data-preparation/merge_data.R
@@ -1,6 +1,6 @@
 # Load datasets into R 
-Dallas_listings <- read_csv('data/Dallas_listings.csv')
-Dallas_reviews <- read_csv('data/Dallas_reviews.csv')
+Dallas_listings <- read_csv('data/Dallas_listings.csv.gz')
+Dallas_reviews <- read_csv('data/Dallas_reviews.csv.gz')
 
 install.packages('dplyr')
 library(dplyr)
@@ -11,3 +11,19 @@ Dallas_reviews <- Dallas_reviews %>%
   rename('id' = listing_id)
 
 Dallas <- left_join(Dallas_listings, Dallas_reviews, by = 'id')
+
+# Load datasets into R 
+Mexico_listings <- read_csv('data/Mexico_listings.csv.gz')
+Mexico_reviews <- read_csv('data/Mexico_reviews.csv.gz')
+
+install.packages('dplyr')
+library(dplyr)
+
+
+#join datasets
+Mexico_reviews <- Mexico_reviews %>%
+  rename('id' = listing_id)
+
+Mexico <- left_join(Mexico_listings, Mexico_reviews, by = 'id')
+
+head(Mexico)
