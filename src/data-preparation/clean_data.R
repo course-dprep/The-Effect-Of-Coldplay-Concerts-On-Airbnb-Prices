1dda964a2efbb252f077a63b61fc82f45d21f1e5
NielsRahder <N.J.Rahder@tilburguniversity.edu>
2022-09-29 12:01:36 +0200

    clean and filter all the data

--- src/data-preparation/clean_data.R
+++ src/data-preparation/clean_data.R
@@ -1,27 +1,55 @@
-###own data####
+read.csv("")
+
+
 #subset 
-Mexico <- subset(Mexico_listings, select = -c (host_name, reviews_per_month, calculated_host_listings_count, license, neighbourhood_group, minimum_nights, number_of_reviews_ltm))
-MexicoDuringConcert <- subset(Mexico$last_review, select = ("03-04-2022":"07-04-2022"))
+Mexico <- Mexico %>% 
+  select(Identifier, host_is_superhost, latitude, longitude, property_type, accommodates, price, date)
 
 #filter dates during concert  
 MexicoDuringConcert <- Mexico %>%
-  filter(between(last_review, as.Date('2022-04-03'), as.Date('2022-04-07'))) %>%
-  arrange(last_review) 
+filter(between(date, as.Date('2022-04-03'), as.Date('2022-04-07'))) %>%
+  arrange(date) 
 
 #filter Dallas 
-Dallas <- subset(Dallas_Listings, select = -c (host_name, reviews_per_month, calculated_host_listings_count, license, neighbourhood_group, minimum_nights, number_of_reviews_ltm))
-MexicoDuringConcert <- subset(Mexico$last_review, select = ("06-04-2022":"06-04-2022"))
+Dallas <- Dallas %>%
+  select(Identifier, host_is_superhost, latitude, longitude, property_type, accommodates, price, date) 
 
 #filter dates during concert  
 DallasDuringConcert <- Dallas %>%
-  filter(between(last_review, as.Date('2022-04-06'), as.Date('2022-04-06'))) %>%
-  arrange(last_review) 
+  filter(between(date, as.Date('2022-04-06'), as.Date('2022-04-06'))) %>%
+  arrange(date)
 
-#clean price column 
+#Filter Chicago 
+Chicago <- Chicago %>%
+  select(Identifier, host_is_superhost, latitude, longitude, property_type, accommodates, price, date)
+
+ChicagoDuringConcert <- Chicago %>%
+  filter(between(date, as.Date('2022-05-28'), as.Date('2022-05-29'))) %>%
+  arrange(date) 
+
+
+
+
+#clean price column Dallas (not pretty but it works)
 Dallas$price <- gsub('[$]', '', Dallas$price)
 Dallas$price <- gsub('[,]', '', Dallas$price)
 Dallas$price <- gsub('[.]', '', Dallas$price)
 Dallas$price <- as.numeric(Dallas$price)
 Dallas$price <- Dallas$price / 100
+
+#clean price column Chicago (not pretty but it works)
+Chicago$price <- gsub('[$]', '', Chicago$price)
+Chicago$price <- gsub('[,]', '', Chicago$price)
+Chicago$price <- gsub('[.]', '', Chicago$price)
+Chicago$price <- as.numeric(Chicago$price)
+Chicago$price <- Chicago$price / 100
+
+#clean price column Mexico (not pretty but it works)
+Mexico$price <- gsub('[$]', '', Mexico$price)
+Mexico$price <- gsub('[,]', '', Mexico$price)
+Mexico$price <- gsub('[.]', '', Mexico$price)
+Mexico$price <- as.numeric(Mexico$price)
+Mexico$price <- Mexico$price / 100
+
 
 
