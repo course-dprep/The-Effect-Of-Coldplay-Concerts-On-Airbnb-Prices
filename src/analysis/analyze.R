4646d18e5457ca465d31e9acdaba3f3043364872
NielsRahder <N.J.Rahder@tilburguniversity.edu>
2022-09-29 14:25:58 +0200

    adjusted for one dataset

--- src/analysis/analyze.R
+++ src/analysis/analyze.R
@@ -5,11 +5,27 @@ install.packages('broom')
 library(fixest)
 library(broom)
 library(tidyr)
+library(ggplot2)
 
 reg1 <-   feols(price ~ distance + 
-                  accommodates,
-              data = MexicoDuringConcert)
+                  accommodates + DuringConcert, 
+                  data = Mexico)
 tidy(reg1, conf.int = TRUE)
+
+
+#create graph with price 
+Mexico %>% ß
+  fil
+         ggplot(aes(x = date,
+                    y = price,
+                    size = price, 
+                    )) + 
+           geom_point()
+
+         
+
+         
+range(Mexico$date, asc)
 
 
 
