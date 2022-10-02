82324dec48f475b9f4f924d1650440ac86664462
NielsRahder <N.J.Rahder@tilburguniversity.edu>
2022-10-02 23:01:34 +0200

    create new directories for the different cities

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -1,27 +1,29 @@
 library(readr)
 
 #create data repository 
-dir.create('../../data')  
+dir.create('../../data/Dallas')  
+dir.create('../../data/Chicago')
+dir.create('../../data/Mexico')
 
 
 #download and load data 
 #Dallas
 download.file(url = 'http://data.insideairbnb.com/united-states/tx/dallas/2022-09-14/data/listings.csv',
-              destfile = '../../data/Dallas_listings.csv')
+              destfile = '../../data/Dallas/Dallas_listings.csv')
 download.file(url = 'http://data.insideairbnb.com/united-states/tx/dallas/2022-09-14/data/reviews.csv',
-              destfile = './data/Dallas_reviews.csv')
+              destfile = './data/Dallas/Dallas_reviews.csv')
 
 #Chicago
 download.file(url = 'http://data.insideairbnb.com/united-states/il/chicago/2022-09-14/data/listings.csv',
-              destfile = '../../data/Chicago_listings.csv')
+              destfile = '../../data/Chicago/Chicago_listings.csv')
 
 download.file(url = 'http://data.insideairbnb.com/united-states/il/chicago/2022-09-14/data/reviews.csv',
-              destfile = '../../data/Chicago_reviews.csv')
+              destfile = '../../data/Chicago/Chicago_reviews.csv')
 
 
 #Mexico City
 download.file(url = 'http://data.insideairbnb.com/mexico/df/mexico-city/2022-06-21/data/listings.csv',
-              destfile = '../../data/Mexico_listings.csv')
+              destfile = '../../data/Mexico/Mexico_listings.csv')
 download.file(url = 'http://data.insideairbnb.com/mexico/df/mexico-city/2022-06-21/data/reviews.csv',
-              destfile = '../../data/Mexico_reviews.csv')
+              destfile = '../../data/Mexico/Mexico_reviews.csv')
 
