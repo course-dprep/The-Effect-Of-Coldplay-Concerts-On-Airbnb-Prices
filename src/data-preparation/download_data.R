bd596e5829dd77d5285d07076c4f9816b806ef4f
NielsRahder <N.J.Rahder@tilburguniversity.edu>
2022-10-02 22:26:21 +0200

    fix gs and correct directions

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -1,27 +1,27 @@
 library(readr)
 
 #create data repository 
-dir.create('./data')  
+dir.create('../../data')  
 
 
 #download and load data 
 #Dallas
-download.file(url = 'http://data.insideairbnb.com/united-states/tx/dallas/2022-09-14/data/listings.csv.gz',
-              destfile = './data/Dallas_listings.csv.gz')
-download.file(url = 'http://data.insideairbnb.com/united-states/tx/dallas/2022-09-14/data/reviews.csv.gz',
-              destfile = './data/Dallas_reviews.csv.gz')
+download.file(url = 'http://data.insideairbnb.com/united-states/tx/dallas/2022-09-14/data/listings.csv',
+              destfile = '../../data/Dallas_listings.csv')
+download.file(url = 'http://data.insideairbnb.com/united-states/tx/dallas/2022-09-14/data/reviews.csv',
+              destfile = './data/Dallas_reviews.csv')
 
 #Chicago
-download.file(url = 'http://data.insideairbnb.com/united-states/il/chicago/2022-09-14/data/listings.csv.gz',
-              destfile = './data/Chicago_listings.csv.gz')
+download.file(url = 'http://data.insideairbnb.com/united-states/il/chicago/2022-09-14/data/listings.csv',
+              destfile = '../../data/Chicago_listings.csv')
 
-download.file(url = 'http://data.insideairbnb.com/united-states/il/chicago/2022-09-14/data/reviews.csv.gz',
-              destfile = './data/Chicago_reviews.csv.gz')
+download.file(url = 'http://data.insideairbnb.com/united-states/il/chicago/2022-09-14/data/reviews.csv',
+              destfile = '../../data/Chicago_reviews.csv')
 
 
 #Mexico City
-download.file(url = 'http://data.insideairbnb.com/mexico/df/mexico-city/2022-06-21/data/listings.csv.gz',
-              destfile = './data/Mexico_listings.csv.gz')
-download.file(url = 'http://data.insideairbnb.com/mexico/df/mexico-city/2022-06-21/data/reviews.csv.gz',
-              destfile = './data/Mexico_reviews.csv.gz')
+download.file(url = 'http://data.insideairbnb.com/mexico/df/mexico-city/2022-06-21/data/listings.csv',
+              destfile = '../../data/Mexico_listings.csv')
+download.file(url = 'http://data.insideairbnb.com/mexico/df/mexico-city/2022-06-21/data/reviews.csv',
+              destfile = '../../data/Mexico_reviews.csv')
 
