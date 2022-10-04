52a336d9f29a9277aeb98f44ad47be9ad6f14f84
NielsRahder <N.J.Rahder@tilburguniversity.edu>
2022-10-04 18:17:50 +0200

    fix na.omit() bug

--- src/data-preparation/clean_data.R
+++ src/data-preparation/clean_data.R
@@ -10,63 +10,59 @@ Chicago <- read_csv("../../data/Chicago/Chicago_merged.csv")
 
 
 #subset, filter for outlying prices and create dummy for dates during the concert for Mexico
-Mexico <- Mexico %>% 
+Mexico_cleaned <- Mexico %>% 
   select(Identifier, host_is_superhost, latitude, longitude, room_type, accommodates, price, date) %>%
   na.omit() %>%
   filter(price < 9999)
 
-Mexico$DuringConcert <- ifelse(Mexico$date == as.Date("2022-05-28") | Mexico$date == as.Date("2022-05-29"), 1, 0) 
+Mexico_cleaned$DuringConcert <- ifelse(Mexico$date == as.Date("2022-05-28") | Mexico$date == as.Date("2022-05-29"), 1, 0) 
 
 
 #subset, filter for outlying prices and create dummy for dates during the concert for Dallas
-Dallas <- Dallas %>%
+Dallas_cleaned <- Dallas %>%
   select(Identifier, host_is_superhost, latitude, longitude, room_type, accommodates, price, date) %>%
   na.omit() %>%
   filter(price < 9999)
   
-Dallas$DuringConcert <- ifelse(Dallas$date == as.Date("2022-05-06"), 1, 0) 
+Dallas_cleaned$DuringConcert <- ifelse(Dallas$date == as.Date("2022-05-06"), 1, 0) 
 
 
 
 #subset, filter for outlying prices and create dummy for dates during the concert for Chicago 
-Chicago <- Chicago %>%
+Chicago_cleaned <- Chicago %>%
   select(Identifier, host_is_superhost, latitude, longitude, room_type, accommodates, price, date) %>%
   na.omit() %>%
   filter(price < 9999)
 
-Chicago$DuringConcert <- ifelse(Chicago$date == as.Date("2022-05-28") | Chicago$date == as.Date("2022-05-29"), 1, 0) 
+Chicago_cleaned$DuringConcert <- ifelse(Chicago$date == as.Date("2022-05-28") | Chicago$date == as.Date("2022-05-29"), 1, 0) 
 
 
 #clean price column Dallas (not pretty but it works)
-Dallas$price <- gsub('[$]', '', Dallas$price)
-Dallas$price <- gsub('[,]', '', Dallas$price)
-Dallas$price <- gsub('[.]', '', Dallas$price)
-Dallas$price <- as.numeric(Dallas$price)
-Dallas$price <- Dallas$price / 100
+Dallas_cleaned$price <- gsub('[$]', '', Dallas_cleaned$price)
+Dallas_cleaned$price <- gsub('[,]', '', Dallas_cleaned$price)
+Dallas_cleaned$price <- gsub('[.]', '', Dallas_cleaned$price)
+Dallas_cleaned$price <- as.numeric(Dallas_cleaned$price)
+Dallas_cleaned$price <- Dallas_cleaned$price / 100
 
 #clean price column Chicago (not pretty but it works)
-Chicago$price <- gsub('[$]', '', Chicago$price)
-Chicago$price <- gsub('[,]', '', Chicago$price)
-Chicago$price <- gsub('[.]', '', Chicago$price)
-Chicago$price <- as.numeric(Chicago$price)
-Chicago$price <- Chicago$price / 100
+Chicago_cleaned$price <- gsub('[$]', '', Chicago_cleaned$price)
+Chicago_cleaned$price <- gsub('[,]', '', Chicago_cleaned$price)
+Chicago_cleaned$price <- gsub('[.]', '', Chicago_cleaned$price)
+Chicago_cleaned$price <- as.numeric(Chicago_cleaned$price)
+Chicago_cleaned$price <- Chicago_cleaned$price / 100
 
 #clean price column Mexico (not pretty but it works)
-Mexico$price <- gsub('[$]', '', Mexico$price)
-Mexico$price <- gsub('[,]', '', Mexico$price)
-Mexico$price <- gsub('[.]', '', Mexico$price)
-Mexico$price <- as.numeric(Mexico$price)
-Mexico$price <- Mexico$price / 100 
+Mexico_cleaned$price <- gsub('[$]', '', Mexico_cleaned$price)
+Mexico_cleaned$price <- gsub('[,]', '', Mexico_cleaned$price)
+Mexico_cleaned$price <- gsub('[.]', '', Mexico_cleaned$price)
+Mexico_cleaned$price <- as.numeric(Mexico_cleaned$price)
+Mexico_cleaned$price <- Mexico_cleaned$price / 100 
 
 #create dummy variable for is host_superhost 
-Mexico$host_is_superhost <- as.numeric(Mexico$host_is_superhost)
-Dallas$host_is_superhost <- as.numeric(Dallas$host_is_superhost)
-Chicago$host_is_superhost <- as.numeric(Chicago$host_is_superhost)
+Mexico_cleaned$host_is_superhost <- as.numeric(Mexico_cleaned$host_is_superhost)
+Dallas_cleaned$host_is_superhost <- as.numeric(Dallas_cleaned$host_is_superhost)
+Chicago_cleaned$host_is_superhost <- as.numeric(Chicago_cleaned$host_is_superhost)
 
-#rename for clarity 
-Mexico_cleaned <- Mexico
-Chicaco_cleaned <- Chicago
-Dallas_cleaned <- Dallas
 
 #output
 fwrite(Mexico, file = "../../data/Mexico/Mexico_cleaned.csv", sep = ",", quote = TRUE)
