567114124ce66c94e6b7d20ee580ccc0db8b52f7
Eveline de Veld <e.e.m.develd@tilburguniversity.edu>
2022-10-06 13:47:32 +0200

    Modified filter for price.

--- src/data-preparation/clean_data.R
+++ src/data-preparation/clean_data.R
@@ -11,16 +11,14 @@ Chicago <- read_csv("../../data/Chicago/Chicago_merged.csv")
 
 #subset, filter for outlying prices and create dummy for dates during the concert for Mexico
 Mexico_cleaned <- Mexico %>% 
-  select(Identifier, host_is_superhost, latitude, longitude, room_type, accommodates, price, date) %>%
-  filter(price < 9999)
+  select(Identifier, host_is_superhost, latitude, longitude, room_type, accommodates, price, date) 
 
 Mexico_cleaned$DuringConcert <- ifelse(Mexico$date == as.Date("2022-05-28") | Mexico$date == as.Date("2022-05-29"), 1, 0) 
 
 
 #subset, filter for outlying prices and create dummy for dates during the concert for Dallas
 Dallas_cleaned <- Dallas %>%
-  select(Identifier, host_is_superhost, latitude, longitude, room_type, accommodates, price, date) %>%
-  filter(price < 9999)
+  select(Identifier, host_is_superhost, latitude, longitude, room_type, accommodates, price, date) 
   
 Dallas_cleaned$DuringConcert <- ifelse(Dallas$date == as.Date("2022-05-06"), 1, 0) 
 
@@ -28,8 +26,7 @@ Dallas_cleaned$DuringConcert <- ifelse(Dallas$date == as.Date("2022-05-06"), 1, 
 
 #subset, filter for outlying prices and create dummy for dates during the concert for Chicago 
 Chicago_cleaned <- Chicago %>%
-  select(Identifier, host_is_superhost, latitude, longitude, room_type, accommodates, price, date) %>%
-  filter(price < 9999)
+  select(Identifier, host_is_superhost, latitude, longitude, room_type, accommodates, price, date) 
 
 Chicago_cleaned$DuringConcert <- ifelse(Chicago$date == as.Date("2022-05-28") | Chicago$date == as.Date("2022-05-29"), 1, 0) 
 
@@ -39,26 +36,33 @@ Dallas_cleaned$price <- gsub('[$]', '', Dallas_cleaned$price)
 Dallas_cleaned$price <- gsub('[,]', '', Dallas_cleaned$price)
 Dallas_cleaned$price <- gsub('[.]', '', Dallas_cleaned$price)
 Dallas_cleaned$price <- as.numeric(Dallas_cleaned$price)
-Dallas_cleaned$price <- Dallas_cleaned$price / 100
+
 
 #clean price column Chicago (not pretty but it works)
 Chicago_cleaned$price <- gsub('[$]', '', Chicago_cleaned$price)
 Chicago_cleaned$price <- gsub('[,]', '', Chicago_cleaned$price)
 Chicago_cleaned$price <- gsub('[.]', '', Chicago_cleaned$price)
 Chicago_cleaned$price <- as.numeric(Chicago_cleaned$price)
-Chicago_cleaned$price <- Chicago_cleaned$price / 100
+
 
 #clean price column Mexico (not pretty but it works)
 Mexico_cleaned$price <- gsub('[$]', '', Mexico_cleaned$price)
 Mexico_cleaned$price <- gsub('[,]', '', Mexico_cleaned$price)
 Mexico_cleaned$price <- gsub('[.]', '', Mexico_cleaned$price)
 Mexico_cleaned$price <- as.numeric(Mexico_cleaned$price)
-Mexico_cleaned$price <- Mexico_cleaned$price / 100 
 
 #create dummy variable for is host_superhost 
 Mexico_cleaned$host_is_superhost <- as.numeric(Mexico_cleaned$host_is_superhost)
 Dallas_cleaned$host_is_superhost <- as.numeric(Dallas_cleaned$host_is_superhost)
 Chicago_cleaned$host_is_superhost <- as.numeric(Chicago_cleaned$host_is_superhost)
+
+#filter outliers by filtering price < 9999 #check if this is necessary 
+# Dallas_cleaned <- Dallas_cleaned %>% 
+  #filter(price < 9999)
+# Chicago_cleaned <- Chicago_cleaned %>%
+  #filter(price < 9999)
+#Mexico_cleaned <- Mexico_cleaned %>%
+  #filter(price < 9999)
 
 
 #output
