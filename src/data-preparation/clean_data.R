79207ae37027607ad42e47f82db118b85f0994c6
NielsRahder <N.J.Rahder@tilburguniversity.edu>
2022-10-07 23:29:19 +0200

    put distance measure in correct place

--- src/data-preparation/clean_data.R
+++ src/data-preparation/clean_data.R
@@ -68,6 +68,40 @@ Mexico_cleaned_for_dist <- Mexico_cleaned
 Dallas_cleaned_for_dist <- Dallas_cleaned
 Chicago_cleaned_for_dist <- Chicago_cleaned
 
+#add distance column 
+earth.dist <- function (long1, lat1, long2, lat2)
+{
+  rad <- pi/180
+  a1 <- lat1 * rad
+  a2 <- long1 * rad
+  b1 <- lat2 * rad
+  b2 <- long2 * rad
+  dlon <- b2 - a2
+  dlat <- b1 - a1
+  a <- (sin(dlat/2))^2 + cos(a1) * cos(b1) * (sin(dlon/2))^2
+  c <- 2 * atan2(sqrt(a), sqrt(1 - a))
+  R <- 6378.145
+  d <- R * c
+  return(d)
+}
+
+#coordinates of Cotton Bowl stadium (Dalles) are -96.452099, 32.462819
+
+Dallas_cleaned_for_dist$distance <- earth.dist(Dallas_cleaned_for_dist$longitude, Dallas_cleaned$latitude, -96.452099, 32.462819)
+
+#coordinates of Foro Sol (Mexico City) are -99.052724, 19.241055 
+
+Mexico_cleaned_for_dist$distance <- earth.dist(Mexico_cleaned_for_dist$longitude, Mexico_cleaned$latitude, -99.052724, 19.241055)
+
+#coordinates of Soldier Field (Chicago) are -87.617256, 41.862366
+
+Chicago_cleaned_for_dist$distance <- earth.dist(Chicago_cleaned_for_dist$longitude, Chicago_cleaned$latitude, -87.617256, 41.862366)
+
+#write output
+fwrite(Mexico_cleaned_for_dist, file = "../../data/Mexico/Mexico_cleaned_for_dist")
+fwrite(Dallas_cleaned_for_dist, file = "../../data/Dallas/Dallas_cleaned_for_dist")
+fwrite(Chicago_cleaned_for_dist, file = "../../data/Chicago/Chicago_cleaned_for_dist")
+
 #output
 fwrite(Mexico_cleaned, file = "../../data/Mexico/Mexico_cleaned_for_dist.csv", sep = ",", quote = TRUE)
 fwrite(Dallas_cleaned, file = "../../data/Dallas/Dallas_cleaned_for_dist.csv", sep = ",", quote = TRUE)
