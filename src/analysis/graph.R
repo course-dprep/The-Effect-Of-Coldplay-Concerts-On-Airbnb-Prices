f8da3fd1cac38f417d52b380428f69cf59b91e14
NielsRahder <N.J.Rahder@tilburguniversity.edu>
2022-10-08 01:09:36 +0200

    added some graphs, still have to finetune

--- src/analysis/graph.R
+++ src/analysis/graph.R
@@ -1,32 +1,43 @@
 #code for measuring distance 
 
-Mexico_cleaned_for_dist <- read_csv("../../data//Mexico/Mexico_cleaned.csv")
-Dallas_cleaned_for_dist <- read_csv("../../data/Dallas/Dallas_cleaned.csv")
-Chicago_cleaned_for_dist <- read_csv("../../data/Chicago/Chicago_cleaned.csv")
+Mexico_cleaned <- read_csv("../../data//Mexico/Mexico_cleaned.csv")
+Dallas_cleaned <- read_csv("../../data/Dallas/Dallas_cleaned.csv")
+Chicago_cleaned <- read_csv("../../data/Chicago/Chicago_cleaned.csv")
 
 #create graph with mean price for Dallas
-Dallas_graph <- Dallas_cleaned %>% 
-  filter(date >= "2022-04-06") %>%
-  filter(date <= "2022-06-06") %>%
+Dallas_graph <- Dallas_cleaned %>% filter(distance <= 50) %>% 
+  filter(date >= "2022-05-01") %>%
+  filter(date <= "2022-05-10") %>%
+  group_by(date) %>%
   summarize(mean_price = mean(price)) %>%
   ggplot(aes(x = date, y = mean_price)) + 
-  geom_line(color = 'blue')
+  geom_line(color = 'blue') + 
+  geom_hline(yintercept = mean(Dallas_cleaned$price), color="red")
 print(Dallas_graph)
 
 Dallas_point_graph <- Dallas_cleaned %>%
   filter(date >= "2022-04-06") %>%
   filter(date <= "2022-06-06") %>%
-  ggplot(aes(x=date, y=price, group=DuringConcert))+
-  geom_point(aes(color=DuringConcert))
+  summarise(mean_price = mean(price)) %>%
+  ggplot(aes(x= date, y= mean_price)) +
+  geom_point()
 print(Dallas_point_graph)
 
+Dallas_cleaned %>% filter(price < 9999) %>% 
+  filter(date >= "2022-04-06") %>%
+  filter(date <= "2022-06-06") %>%
+  ggplot(Dallas_cleaned, mapping = aes(date, price)) + geom_point(aes(color = distance))
+
+class(Mexico_cleaned$distance)
 #create graph with mean price for Mexico
-Mexico_graph <- Mexico_cleaned %>% group_by(date) %>%
+Mexico_graph <- Mexico_cleaned %>% filter(distance <= 50) %>% 
+  filter(date >= "2022-04-01") %>%
+  filter(date <= "2022-04-10") %>%
+  group_by(date) %>%
   summarize(mean_price = mean(price)) %>%
-  filter(DuringConcert == 1) %>%
-  filter(distance <= 25) %>%
   ggplot(aes(x = date, y = mean_price)) + 
-  geom_line(color = 'blue')
+  geom_line(color = 'blue') + 
+  geom_hline(yintercept = mean(Mexico_cleaned$price), color="red")
 print(Mexico_graph)
 
 fwrite(Dallas_graph, file = "../../gen/Dallas_graph")
