ca8768f7836301f6fdffbe7b0ee92226502eaeb7
NielsRahder <N.J.Rahder@tilburguniversity.edu>
2022-10-07 23:33:47 +0200

    remove graph code

--- src/analysis/analyze.R
+++ src/analysis/analyze.R
@@ -35,32 +35,4 @@ fwrite(Dallas_reg, file = "../../gen/Dallas_reg")
 fwrite(Mexico_reg, file = "../../gen/Mexico_reg")
 fwrite(Chicago_reg, file = "../../gen/Chicago_reg")
 
-#create graph with mean price for Dallas
-Dallas_graph <- Dallas_cleaned %>% 
-  filter(date >= "2022-04-06") %>%
-  filter(date <= "2022-06-06") %>%
-  summarize(mean_price = mean(price)) %>%
-    ggplot(aes(x = date, y = mean_price)) + 
-  geom_line(color = 'blue')
-print(Dallas_graph)
 
-Dallas_point_graph <- Dallas_cleaned %>%
-  filter(date >= "2022-04-06") %>%
-  filter(date <= "2022-06-06") %>%
-  ggplot(aes(x=date, y=price, group=DuringConcert))+
-  geom_point(aes(color=DuringConcert))
-print(Dallas_point_graph)
-
-#create graph with mean price for Mexico
-Mexico_graph <- Mexico_cleaned %>% group_by(date) %>%
-  summarize(mean_price = mean(price)) %>%
-  filter(DuringConcert == 1) %>%
-  filter(distance <= 25) %>%
-  ggplot(aes(x = date, y = mean_price)) + 
-  geom_line(color = 'blue')
-print(Mexico_graph)
-
-fwrite(Dallas_graph, file = "../../gen/Dallas_graph")
-fwrite(Mexico_graph, file = "../../gen/Mexico_graph")
-fwirte(Chicago_graph, file = "../../gen/Chicago_graph" )
-         
