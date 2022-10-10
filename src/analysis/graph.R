6b3849e66ec558392f4f4f0d54fd903f6308b8b9
Thomas van Dijk <thomasvandijk@ww080810.uvt.nl>
2022-10-10 17:57:26 +0200

    removed dates & prices -> otherwise double. Data room_type in ggplot

--- src/analysis/graph.R
+++ src/analysis/graph.R
@@ -7,8 +7,6 @@ Chicago_cleaned <- read_csv("../../data/Chicago/Chicago_cleaned.csv")
 
 #create graph with mean price for Dallas
 Dallas_graph <- Dallas_cleaned %>% filter(distance <= 50) %>% 
-  filter(date >= "2022-04-06") %>%
-  filter(date <= "2022-06-06") %>%
   group_by(date) %>%
   summarize(mean_price = mean(price)) %>%
   ggplot(aes(x = date, y = mean_price)) + 
@@ -17,22 +15,18 @@ Dallas_graph <- Dallas_cleaned %>% filter(distance <= 50) %>%
 print(Dallas_graph)
 
 Dallas_point_graph <- Dallas_cleaned %>%
-  filter(date >= "2022-04-06") %>%
-  filter(date <= "2022-06-06") %>%
   summarise(mean_price = mean(price)) %>%
   ggplot(aes(x= date, y= mean_price)) +
   geom_point()
 print(Dallas_point_graph)
 
-Dallas_cleaned %>% filter(price < 9999) %>% 
-  filter(date >= "2022-04-06") %>%
-  filter(date <= "2022-06-06") %>%
+
+Dallas_cleaned %>%
+  filter(DuringConcert) %>%
   ggplot(Dallas_cleaned, mapping = aes(date, price)) + geom_point(aes(color = distance))
 
 #create graph with mean price for Mexico
 Mexico_graph <- Mexico_cleaned %>% filter(distance <= 50) %>% 
-  filter(date >= "2022-03-03") %>%
-  filter(date <= "2022-05-07") %>%
   group_by(date) %>%
   summarize(mean_price = mean(price)) %>%
   ggplot(aes(x= date, y= mean_price)) + 
@@ -41,22 +35,17 @@ Mexico_graph <- Mexico_cleaned %>% filter(distance <= 50) %>%
 print(Mexico_graph)
 
 Mexico_point_graph <- Mexico_cleaned %>%
-  filter(date >= "2022-03-03") %>%
-  filter(date <= "2022-05-07") %>%
   summarise(mean_price = mean(price)) %>%
   ggplot(aes(x= date, y= mean_price)) +
   geom_point()
 print(Mexico_point_graph)
 
-Mexico_cleaned %>% filter(price < 9999) %>% 
-  filter(date >= "2022-03-03") %>%
-  filter(date <= "2022-05-07") %>%
-  ggplot(Mexico_cleaned, mapping = aes(date, price)) + geom_point(aes(color = distance))
+Mexico_cleaned %>% 
+  filter(DuringConcert)
+  ggplot(Mexico_cleaned, mapping = aes(room_type, price)) + geom_point(aes(color = distance))
 
 #create graph with mean price for Chicago
 Chicago_graph <- Chicago_cleaned %>% filter(distance <= 50) %>% 
-  filter(date >= "2022-04-28") %>%
-  filter(date <= "2022-06-29") %>%
   group_by(date) %>%
   summarize(mean_price = mean(price)) %>%
   ggplot(aes(x= date, y= mean_price)) + 
@@ -65,17 +54,14 @@ Chicago_graph <- Chicago_cleaned %>% filter(distance <= 50) %>%
 print(Chicago_graph)
 
 Chicago_point_graph <- Chicago_cleaned %>%
-  filter(date >= "2022-04-28") %>%
-  filter(date <= "2022-06-29") %>%
   summarise(mean_price = mean(price)) %>%
   ggplot(aes(x= date, y= mean_price)) +
   geom_point()
 print(Chicago_point_graph)
 
-Chicago_cleaned %>% filter(price < 9999) %>% 
-  filter(date >= "2022-04-28") %>%
-  filter(date <= "2022-06-29") %>%
-  ggplot(Chicago_cleaned, mapping = aes(date, price)) + geom_point(aes(color = distance))
+Chicago_cleaned %>%
+  filter(DuringConcert)
+  ggplot(Chicago_cleaned, mapping = aes(room_type, price)) + geom_point(aes(color = distance))
 
 fwrite(Dallas_graph, file = "../../gen/Dallas_graph")
 fwrite(Mexico_graph, file = "../../gen/Mexico_graph")
