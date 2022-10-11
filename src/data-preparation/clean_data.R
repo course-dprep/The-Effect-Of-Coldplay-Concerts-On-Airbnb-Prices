58b028ca2f936b151ea91d11e46f4cbd798c56bd
Eveline de Veld <e.e.m.develd@tilburguniversity.edu>
2022-10-11 14:14:37 +0200

    Organizing the clean_data.R file

--- src/data-preparation/clean_data.R
+++ src/data-preparation/clean_data.R
@@ -8,33 +8,6 @@ Mexico_merged <- read_csv("../../data/Mexico/Mexico_merged.csv")
 Dallas_merged <- read_csv("../../data/Dallas/Dallas_merged.csv")
 Chicago_merged <- read_csv("../../data/Chicago/Chicago_merged.csv")
 
-
-#subset, filter for dates one month before and after the concert for Mexico.
-Mexico_cleaned <- Mexico_merged %>% 
-  select(Identifier, host_is_superhost, latitude, longitude, room_type, accommodates, price, date) %>%
-  filter(Mexico_merged$date > "2022-03-03" & Mexico_merged$date < "2022-05-07")
-
-#create variable for days of the concert for Mexico.
-Mexico_cleaned$DuringConcert <- Mexico_cleaned$date == as.Date("2022-04-03") | Mexico_cleaned$date == as.Date("2022-04-04") | Mexico_cleaned$date == as.Date("2022-04-06")| Mexico_cleaned$date == as.Date("2022-04-07") 
-
-
-#subset, filter for dates one month before and after the concert for Dallas.
-Dallas_cleaned <- Dallas_merged %>%
-  select(Identifier, host_is_superhost, latitude, longitude, room_type, accommodates, price, date) %>%
-  filter(Dallas_merged$date > "2022-04-06" & Dallas_merged$date < "2022-06-06")
-
-#create variable for days of the concert for Dallas
-
-Dallas_cleaned$DuringConcert <- Dallas_cleaned$date == as.Date("2022-05-06")
-
-
-#subset, filter for dates one month before and after the concert for Chicago.
-Chicago_cleaned <- Chicago_merged %>%
-  select(Identifier, host_is_superhost, latitude, longitude, room_type, accommodates, price, date) %>%
-  filter(Chicago_merged$date > "2022-04-28" & Chicago_merged$date < "2022-06-29")
-
-Chicago_cleaned$DuringConcert <- Chicago_cleaned$date == as.Date("2022-05-28") | Chicago_cleaned$date == as.Date("2022-05-29")
-
 #clean price column Dallas
 Dallas_cleaned$price <- gsub('[$]', '', Dallas_cleaned$price)
 Dallas_cleaned$price <- gsub('[,]', '', Dallas_cleaned$price)
@@ -57,17 +30,22 @@ Mexico_cleaned$price <- gsub('[.]', '', Mexico_cleaned$price)
 Mexico_cleaned$price <- as.numeric(Mexico_cleaned$price)
 Mexico_cleaned$price <- Mexico_cleaned$price / 100
 
-#create dummy variable for is host_superhost 
-Mexico_cleaned$host_is_superhost <- as.numeric(Mexico_cleaned$host_is_superhost)
-Dallas_cleaned$host_is_superhost <- as.numeric(Dallas_cleaned$host_is_superhost)
-Chicago_cleaned$host_is_superhost <- as.numeric(Chicago_cleaned$host_is_superhost)
+#subset, filter for dates one month before and after the concert for Mexico and filter out price outliers.
+Mexico_cleaned <- Mexico_merged %>% 
+  select(Identifier, latitude, longitude, room_type, accommodates, price, date) %>%
+  filter(Mexico_merged$date > "2022-03-03" & Mexico_merged$date < "2022-05-07") %>%
+  filter(price < 9999)
 
-# filter outliers by filtering price < 9999 
-Dallas_cleaned <- Dallas_cleaned %>% 
+#subset, filter for dates one month before and after the concert for Dallas and filter out price outliers..
+Dallas_cleaned <- Dallas_merged %>%
+  select(Identifier, latitude, longitude, room_type, accommodates, price, date) %>%
+  filter(Dallas_merged$date > "2022-04-06" & Dallas_merged$date < "2022-06-06") %>%
   filter(price < 9999)
-Chicago_cleaned <- Chicago_cleaned %>%
-  filter(price < 9999)
-Mexico_cleaned <- Mexico_cleaned %>%
+
+#subset, filter for dates one month before and after the concert for Chicago and filter out price outliers..
+Chicago_cleaned <- Chicago_merged %>%
+  select(Identifier, latitude, longitude, room_type, accommodates, price, date) %>%
+  filter(Chicago_merged$date > "2022-04-28" & Chicago_merged$date < "2022-06-29") %>%
   filter(price < 9999)
 
 #calculate distance column 
@@ -87,14 +65,18 @@ earth.dist <- function (long1, lat1, long2, lat2)
   return(d)
 }
 
+# create variables for dates during the concert and distance
 
-#coordinates of Cotton Bowl stadium (Dalles) are -96.452099, 32.462819
-Dallas_cleaned$distance <- earth.dist(Dallas_cleaned$longitude, Dallas_cleaned$latitude, -96.452099, 32.462819)
-
-#coordinates of Foro Sol (Mexico City) are -99.052724, 19.241055 
+# Mexico (coordinates of Foro Sol are -99.052724, 19.241055)
+Mexico_cleaned$DuringConcert <- Mexico_cleaned$date == as.Date("2022-04-03") | Mexico_cleaned$date == as.Date("2022-04-04") | Mexico_cleaned$date == as.Date("2022-04-06")| Mexico_cleaned$date == as.Date("2022-04-07") 
 Mexico_cleaned$distance <- earth.dist(Mexico_cleaned$longitude, Mexico_cleaned$latitude, -99.052724, 19.241055)
 
-#coordinates of Soldier Field (Chicago) are -87.617256, 41.862366
+# Dallas (coordinates of Cotton Bowl stadium are -96.452099, 32.462819)
+Dallas_cleaned$DuringConcert <- Dallas_cleaned$date == as.Date("2022-05-06")
+Dallas_cleaned$distance <- earth.dist(Dallas_cleaned$longitude, Dallas_cleaned$latitude, -96.452099, 32.462819)
+
+# Chicago (coordinates of Soldier Field are -87.617256, 41.862366)
+Chicago_cleaned$DuringConcert <- Chicago_cleaned$date == as.Date("2022-05-28") | Chicago_cleaned$date == as.Date("2022-05-29")
 Chicago_cleaned$distance <- earth.dist(Chicago_cleaned$longitude, Chicago_cleaned$latitude, -87.617256, 41.862366)
 
 #write output
