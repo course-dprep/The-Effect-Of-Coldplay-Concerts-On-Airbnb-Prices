1563307532d6cc025bf5a1543f48ff23fd6712a6
NielsRahder <N.J.Rahder@tilburguniversity.edu>
2022-10-13 23:00:15 +0200

    merge all plots in one file

--- src/analysis/graph.R
+++ src/analysis/graph.R
@@ -2,6 +2,7 @@
 library(ggplot2)
 library(readr)
 library(dplyr)
+library(ggpubr)
 
 #create directory
 dir.create('../../gen/graph')
@@ -16,78 +17,64 @@ Chicago_cleaned <- read_csv("../../data/Chicago/Chicago_cleaned.csv")
 
 #############Dallas####################
 #create graph with mean price for Dallas
-pdf(file = '../../gen/graph/Dallas/Dallas_mean_price_graph.pdf')
-Dallas_cleaned %>% filter(distance <= 50) %>% 
+Dallas_mean_price_graph <- Dallas_cleaned %>% filter(distance <= 50) %>% 
   group_by(date) %>%
   summarize(mean_price = mean(price)) %>%
   ggplot(aes(x = date, y = mean_price)) + 
   geom_line(color = 'blue') + 
   geom_hline(yintercept = mean(Dallas_cleaned$price), color="red")
-dev.off()
-  # create graph with differences in price for different distances on the date of the concert for Dallas
-pdf('../../gen/graph/Dallas/Dallas_distance_price_graph.pdf')
-ggplot(Dallas_cleaned %>% filter(price < 9999) %>% 
+
+# create graph with differences in price for different distances on the date of the concert for Dallas
+Dallas_distance_price_graph <- ggplot(Dallas_cleaned %>% filter(price < 9999) %>% 
            filter(DuringConcert), mapping = aes(distance, price)) + geom_point(aes(size = price, alpha = 0, colour = "red")) + 
   geom_hline(yintercept = mean(Dallas_cleaned$price, na.rm=TRUE))
-dev.off()
 
 # create bar graph with differences in price for different room types for Dallas
-pdf('../../gen/graph/Dallas/Dallas_room_type_graph.pdf')
-Dallas_cleaned %>% 
+Dallas_room_type_graph <- Dallas_cleaned %>% 
   filter(DuringConcert) %>% filter(price < 9999) %>% ggplot(aes(x = room_type, y = mean(price))) + 
   geom_bar(stat = "identity", fill = 'red')
-dev.off()
+
 
 #############Mexico####################
 #create graph with mean price for mexico
-pdf('../../gen/graph/Mexico/Mexico_mean_price_graph.pdf')
-Mexico_cleaned %>% filter(distance <= 50) %>% 
+Mexico_mean_price_graph <- Mexico_cleaned %>% filter(distance <= 50) %>% 
   group_by(date) %>%
   summarize(mean_price = mean(price)) %>%
   ggplot(aes(x = date, y = mean_price)) + 
   geom_line(colour = 'green') + 
   geom_hline(yintercept = mean(Mexico_cleaned$price), colour="black")
-dev.off()
 
 # create graph with differences in price for different distances on the date of the concert for Mexico
-pdf('../../gen/graph/Mexico/Mexico_distance_price_graph.pdf')
-ggplot(Mexico_cleaned %>% filter(price < 9999) %>% 
+Mexico_distance_price_graph <- ggplot(Mexico_cleaned %>% filter(price < 9999) %>% 
            filter(DuringConcert), mapping = aes(distance, price)) + geom_point(aes(size = price, colour = 'green', alpha = 0)) + 
   geom_hline(yintercept = mean(Mexico_cleaned$price, na.rm=TRUE))
-jpeg('../../gen/graph/Mexico/Mexico_distance_price_graph.jpeg')
-dev.off()
 
 # create bar graph with differences in price for different room types for Mexico
-pdf('../../gen/graph/Mexico/Mexico_room_type_graph.pdf')
- Mexico_cleaned %>% 
+Mexico_room_type_graph <- Mexico_cleaned %>% 
   filter(DuringConcert) %>% filter(price < 9999) %>% ggplot(aes(x = room_type, y = mean(price))) + 
   geom_bar(stat = "identity", fill = 'green')
-dev.off()
 
 ###########Chicago########################
 #create graph with mean price for Chicago
-pdf('../../gen/graph/Chicago/Chicago_mean_price_graph.pdf')
-Chicago_cleaned %>% filter(distance <= 50) %>% 
+Chicago_mean_price_graph <- Chicago_cleaned %>% filter(distance <= 50) %>% 
   group_by(date) %>%
   summarize(mean_price = mean(price)) %>%
   ggplot(aes(x = date, y = mean_price)) + 
   geom_line(colour = 'blue') + 
   geom_hline(yintercept = mean(Chicago_cleaned$price), colour="black")
-dev.off()
 
 # create graph with differences in price for different distances on the date of the concert for Chicago
-pdf('../../gen/graph/Chicago/Chicago_distance_price_graph.pdf')
-ggplot(Chicago_cleaned %>% filter(price < 9999) %>% 
+Chicago_distance_price_graph <- ggplot(Chicago_cleaned %>% filter(price < 9999) %>% 
            filter(DuringConcert), mapping = aes(distance, price)) + geom_point(aes(size = price, colour = "blue", alpha = 0)) + 
   geom_hline(yintercept = mean(Chicago_cleaned$price, na.rm=TRUE))
-dev.off()
+
 
 # create bar graph with differences in price for different room types for Chicago
-pdf('../../gen/graph/Chicago/Chicago_room_type_graph.pdf')
-Chicago_cleaned %>% 
+Chicago_room_type_graph <- Chicago_cleaned %>% 
   filter(DuringConcert) %>% filter(price < 9999) %>% ggplot(aes(x = room_type, y = mean(price))) + 
   geom_bar(stat = 'identity', fill = 'blue')
-dev.off()
 
 
-
+ggexport(Chicago_mean_price_graph, Chicago_distance_price_graph, Chicago_room_type_graph, filename = '../../gen/graph/Chicago/Chicago_plots.pdf')
+ggexport(Dallas_mean_price_graph, Dallas_distance_price_graph, Dallas_room_type_graph, filename = '../../gen/graph/Dallas/Dallas_plots.pdf')
+ggexport(Mexico_mean_price_graph, Mexico_distance_price_graph, Mexico_room_type_graph, filename = '../../gen/graph/Mexico/Mexico_plots.pdf')
