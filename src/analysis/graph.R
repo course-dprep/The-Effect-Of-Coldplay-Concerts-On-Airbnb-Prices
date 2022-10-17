ab38b7b2f4af18927636584ab18c1ba1697fc1d2
NielsRahder <N.J.Rahder@tilburguniversity.edu>
2022-10-17 21:51:34 +0200

    change colors for different cities

--- src/analysis/graph.R
+++ src/analysis/graph.R
@@ -22,12 +22,12 @@ Dallas_mean_price_graph <- Dallas_cleaned %>% filter(distance <= 50) %>%
   group_by(date) %>%
   summarize(mean_price = mean(price)) %>%
   ggplot(aes(x = date, y = mean_price)) + 
-  geom_line(color = 'blue') + 
-  geom_hline(yintercept = mean(Dallas_cleaned$price), color="red")
+  geom_line(color = 'red') + 
+  geom_hline(yintercept = mean(Dallas_cleaned$price), color="black")
 
 # create graph with differences in price for different distances on the date of the concert for Dallas
 Dallas_distance_price_graph <- ggplot(Dallas_cleaned %>% filter(price < 9999) %>% 
-           filter(DuringConcert), mapping = aes(distance, price)) + geom_point(aes(size = price, alpha = 0, colour = "red")) + 
+           filter(DuringConcert), mapping = aes(distance, price)) + geom_point(aes(size = price, alpha = 0), colour = 'red' ) + 
   geom_hline(yintercept = mean(Dallas_cleaned$price, na.rm=TRUE))
 
 # create bar graph with differences in price for different room types for Dallas
@@ -47,7 +47,7 @@ Mexico_mean_price_graph <- Mexico_cleaned %>% filter(distance <= 50) %>%
 
 # create graph with differences in price for different distances on the date of the concert for Mexico
 Mexico_distance_price_graph <- ggplot(Mexico_cleaned %>% filter(price < 9999) %>% 
-           filter(DuringConcert), mapping = aes(distance, price)) + geom_point(aes(size = price, colour = 'green', alpha = 0)) + 
+           filter(DuringConcert), mapping = aes(distance, price)) + geom_point(aes(size = price, alpha = 0), colour = 'green') + 
   geom_hline(yintercept = mean(Mexico_cleaned$price, na.rm=TRUE))
 
 # create bar graph with differences in price for different room types for Mexico
@@ -66,9 +66,8 @@ Chicago_mean_price_graph <- Chicago_cleaned %>% filter(distance <= 50) %>%
 
 # create graph with differences in price for different distances on the date of the concert for Chicago
 Chicago_distance_price_graph <- ggplot(Chicago_cleaned %>% filter(price < 9999) %>% 
-           filter(DuringConcert), mapping = aes(distance, price)) + geom_point(aes(size = price, colour = "blue", alpha = 0)) + 
+           filter(DuringConcert), mapping = aes(distance, price)) + geom_point(aes(size = price, alpha = 0), colour = 'blue') + 
   geom_hline(yintercept = mean(Chicago_cleaned$price, na.rm=TRUE))
-
 
 # create bar graph with differences in price for different room types for Chicago
 Chicago_room_type_graph <- Chicago_cleaned %>% 
@@ -76,13 +75,13 @@ Chicago_room_type_graph <- Chicago_cleaned %>%
   geom_bar(stat = 'identity', fill = 'blue')
 
 
-ggexport(Chicago_mean_price_graph, filename = '../../gen/output/graph/Chicago/Chicago_mean_price_graph.png')
-ggexport(Dallas_mean_price_graph, filename = '../../gen/output/graph/Dallas/Dallas_mean_price_graph.png')
-ggexport(Mexico_mean_price_graph, filename = '../../gen/output/graph/Mexico/Mexico_mean_price_graph.png')
-ggexport(Chicago_distance_price_graph, filename = '../../gen/output/graph/Chicago/Chicago_distance_price_graph.png')
-ggexport(Dallas_distance_price_graph, filename = '../../gen/output/graph/Dallas/Dallas_distance_price_graph.png')
-ggexport(Mexico_distance_price_graph, filename = '../../gen/output/graph/Mexico/Mexico_distance_price_graph.png')
-ggexport(Chicago_room_type_graph, filename = '../../gen/output/graph/Chicago/Chicago_room_type_graph.png')
-ggexport(Dallas_room_type_graph, filename = '../../gen/output/graph/Dallas/Dallas_room_type_graph.png')
-ggexport(Mexico_room_type_graph, filename = '../../gen/output/graph/Mexico/Mexico_room_type_graph.png')
+ggexport(Chicago_mean_price_graph, filename = '../../gen/output/graph/Chicago/Chicago_mean_price.png')
+ggexport(Dallas_mean_price_graph, filename = '../../gen/output/graph/Dallas/Dallas_mean_price.png')
+ggexport(Mexico_mean_price_graph, filename = '../../gen/output/graph/Mexico/Mexico_mean_price.png')
+ggexport(Chicago_distance_price_graph, filename = '../../gen/output/graph/Chicago/Chicago_distance_price.png')
+ggexport(Dallas_distance_price_graph, filename = '../../gen/output/graph/Dallas/Dallas_distance_pric.png')
+ggexport(Mexico_distance_price_graph, filename = '../../gen/output/graph/Mexico/Mexico_distance_price.png')
+ggexport(Chicago_room_type_graph, filename = '../../gen/output/graph/Chicago/Chicago_room_type.png')
+ggexport(Dallas_room_type_graph, filename = '../../gen/output/graph/Dallas/Dallas_room_type.png')
+ggexport(Mexico_room_type_graph, filename = '../../gen/output/graph/Mexico/Mexico_room_type.png')
 
